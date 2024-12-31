d0ed13528ccca830de2ccb04652599c709ceb5fe
Davi Perazzoli <daviperazzoli28@gmail.com>
2024-12-31 16:29:20 -0300

    Adicionei TODOs

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -5,7 +5,10 @@ import 'app.dart';
 // import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 // import 'package:flutter/foundation.dart';
 
+// TODO Mudar como as tasks aparecem: (em ordem de dueDate) as sem dueDate aparecem antes, com uma legenda
 // TODO Acrescentar a funcionalidade de dar swipe pros lados pra mudar de página
+// TODO Adicionar filtro na TasksPage: apenas done, apenas undone, atrasadas, não atrasadas, etc. (Necessário remover a done tasks)
+// TODO Futuramente, deixar o usuário criar páginas novas, para mostrar tarefas da forma que ele quiser (Através do filtro e escolhendo o ícone da página)
 
 void main() {
   // if (!kIsWeb && (defaultTargetPlatform == TargetPlatform.windows || 
